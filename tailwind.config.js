8376842a870bd2284de91ef1051a988158420f32
Aranya Jana <aranyajana01@gmail.com>
2024-08-14 21:30:45 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,10 @@
-// module.exports = {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on your project structure
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on your project structure
+  ],
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      backdropBlur: ['hover', 'focus'], // Ensure blur utilities are included
-    },
   },
   plugins: [],
 }
