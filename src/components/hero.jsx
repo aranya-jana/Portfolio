6431fc663aef41eaf6f8e1eb90b5d4335ed82dd5
Aranya Jana <aranyajana01@gmail.com>
2024-09-17 09:25:56 +0530

    first commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import profilepic from "../assets/dp2.jpg";
-import pdfCV from "../assets/aranya_jana_cv4.pdf"; // Path to your PDF file
+import pdfCV from "../assets/ARANYA_JANA_Resume.pdf"; // Path to your PDF file
 import { motion } from "framer-motion";
 
 const Hero = () => {
